60e02be8f908db7e50e334bc22cf91c0d173c462
William Durand <will+git@drnd.me>
2022-06-13 21:57:21 +0200

    yacs: handle stdout and stderr separately

---
+++ internal/yacs/log/file.go
@@ -0,0 +1,53 @@
+package log
+
+import (
+	"bufio"
+	"encoding/json"
+	"io"
+	"os"
+	"sync"
+	"time"
+
+	"github.com/sirupsen/logrus"
+)
+
+type LogFile struct {
+	file *os.File
+	sync.Mutex
+}
+
+func NewFile(name string) (*LogFile, error) {
+	file, err := os.OpenFile(name, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0o644)
+	if err != nil {
+		return nil, err
+	}
+
+	return &LogFile{file: file}, nil
+}
+
+func (l *LogFile) Write(p []byte) (int, error) {
+	l.Lock()
+	defer l.Unlock()
+	return l.file.Write(p)
+}
+
+func (l *LogFile) WriteStream(r io.Reader, name string) {
+	scanner := bufio.NewScanner(r)
+
+	for scanner.Scan() {
+		data, err := json.Marshal(map[string]interface{}{
+			"t": time.Now().UTC(),
+			"m": scanner.Text(),
+			"s": name,
+		})
+
+		if err == nil {
+			if _, err := l.Write(append(data, '\n')); err != nil {
+				logrus.WithFields(logrus.Fields{
+					"s":     name,
+					"error": err,
+				}).Warn("failed to write to container log file")
+			}
+		}
+	}
+}
