4e2362c9258546f9b71a0463478e78f6b459e076
William Durand <will+git@drnd.me>
2022-06-15 22:03:23 +0200

    yaman: add shim models

---
+++ internal/yaman/shim/shim.go
@@ -0,0 +1,25 @@
+package shim
+
+import (
+	"github.com/willdurand/containers/internal/yaman/container"
+)
+
+// BaseShim is the base structure for a shim.
+type BaseShim struct {
+	Container *container.Container
+	Opts      ShimOpts
+}
+
+// ShimOpts contains the options that can be passed to a shim.
+type ShimOpts struct {
+	Runtime string
+}
+
+const stateFileName = "shim.json"
+
+// ID returns the ID of the shim, which is also the container's ID given that a
+// shim is bound to a container (or the other way around) and container IDs are
+// unique.
+func (s *BaseShim) ID() string {
+	return s.Container.ID
+}
