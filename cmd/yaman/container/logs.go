81ee96ff5519b662b417a8990ae1635e70cacabe
William Durand <will+git@drnd.me>
2022-06-15 23:09:27 +0200

    yaman: add 'yaman container logs' command

---
+++ cmd/yaman/container/logs.go
@@ -0,0 +1,33 @@
+package container
+
+import (
+	"os"
+
+	"github.com/spf13/cobra"
+	"github.com/willdurand/containers/internal/yaman"
+)
+
+func init() {
+	cmd := &cobra.Command{
+		Use:          "logs <container>",
+		Short:        "Fetch the logs of a container",
+		RunE:         logs,
+		SilenceUsage: true,
+		Args:         cobra.ExactArgs(1),
+	}
+	cmd.Flags().BoolP("timestamps", "t", false, "show timestamps")
+	containerCommand.AddCommand(cmd)
+}
+
+func logs(cmd *cobra.Command, args []string) error {
+	rootDir, _ := cmd.Flags().GetString("root")
+	timestamps, _ := cmd.Flags().GetBool("timestamps")
+
+	opts := yaman.CopyLogsOpts{
+		Timestamps: timestamps,
+		Stdout:     os.Stdout,
+		Stderr:     os.Stderr,
+	}
+
+	return yaman.CopyLogs(rootDir, args[0], opts)
+}
