eb27c66c95dcc98b764305c03da723af13b08042
William Durand <will+git@drnd.me>
2022-06-16 15:32:55 +0200

    yaman: remove cmd/yaman/root.go (#40)

--- cmd/yaman/main.go
+++ cmd/yaman/main.go
@@ -1,7 +1,26 @@
 // Yet Another (Container) MANager.
 package main
 
-import "github.com/willdurand/containers/internal/cli"
+import (
+	"github.com/willdurand/containers/cmd/yaman/container"
+	"github.com/willdurand/containers/cmd/yaman/image"
+	"github.com/willdurand/containers/internal/cli"
+)
+
+const (
+	programName string = "yaman"
+)
+
+// rootCmd represents the root command.
+var rootCmd = cli.NewRootCommand(
+	programName,
+	"Yet another daemonless container manager",
+)
+
+func init() {
+	image.Register(rootCmd)
+	container.Register(rootCmd)
+}
 
 func main() {
 	cli.Execute(rootCmd)
