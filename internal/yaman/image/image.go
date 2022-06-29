09a9fb9abd2d5f8df70058380217648ab85137fe
William Durand <will+git@drnd.me>
2022-06-29 23:04:56 +0200

    yaman: do not lazy load important properties (#108)

--- internal/yaman/image/image.go
+++ internal/yaman/image/image.go
@@ -17,8 +17,8 @@ type Image struct {
 	Name     string
 	Version  string
 	BaseDir  string
-	manifest *imagespec.Manifest
-	config   *imagespec.Image
+	Manifest *imagespec.Manifest
+	Config   *imagespec.Image
 }
 
 const defaultImageVersion = "latest"
@@ -48,12 +48,14 @@ func New(rootDir, name string) (*Image, error) {
 		imageVersion = parts[1]
 	}
 
-	return &Image{
+	img := &Image{
 		Hostname: hostName,
 		Name:     imageName,
 		Version:  imageVersion,
 		BaseDir:  filepath.Join(GetBaseDir(rootDir), hostName, imageName, imageVersion),
-	}, nil
+	}
+
+	return img, nil
 }
 
 // GetBaseDir returns the base directory where all images are stored (locally).
@@ -66,68 +68,20 @@ func (i *Image) FQIN() string {
 	return i.Hostname + "/" + i.Name + ":" + i.Version
 }
 
-// Manifest returns the manifest data of an image (or an error when it fails).
-// The data is loaded from disk and then cached.
-func (i *Image) Manifest() (*imagespec.Manifest, error) {
-	if i.manifest != nil {
-		return i.manifest, nil
-	}
-
-	data, err := os.ReadFile(i.ManifestFilePath())
-	if err != nil {
-		return nil, err
-	}
-
-	manifest := new(imagespec.Manifest)
-	if err := json.Unmarshal(data, manifest); err != nil {
-		return nil, err
-	}
-	i.manifest = manifest
-
-	return i.manifest, nil
-}
-
-// Config returns the image configuration (or an error when it fails). The data
-// is loaded from disk and then cached.
-func (i *Image) Config() (*imagespec.Image, error) {
-	if i.config != nil {
-		return i.config, nil
-	}
-
-	file := filepath.Join(i.ConfigFilePath())
-	data, err := os.ReadFile(file)
-	if err != nil {
-		return nil, err
-	}
-
-	config := new(imagespec.Image)
-	if err := json.Unmarshal(data, config); err != nil {
-		return nil, err
-	}
-	i.config = config
-
-	return i.config, nil
-}
-
 // LayerDirs returns a list of absolute (directory) paths pointing to the
 // different layers of the image. This list is ordered so that the last layer
 // directory in the list is the lowest directory (which matches the expectation
 // of `lowerdir` in Overlay).
 func (i *Image) LayerDirs() []string {
 	var dirs []string
-
-	config, err := i.Config()
-	if err != nil {
-		return dirs
-	}
 	// Note: The order of lower directories is the rightmost is the lowest, thus
 	// the upper directory is on top of the first directory in the left-to-right
 	// list of lower directories; NOT on top of the last directory in the list,
 	// as the order might seem to suggest.
 	//
 	// Source: https://wiki.archlinux.org/title/Overlay_filesystem
-	for idx := len(config.RootFS.DiffIDs) - 1; idx >= 0; idx-- {
-		digest := config.RootFS.DiffIDs[idx]
+	for idx := len(i.Config.RootFS.DiffIDs) - 1; idx >= 0; idx-- {
+		digest := i.Config.RootFS.DiffIDs[idx]
 		dirs = append(dirs, filepath.Join(i.LayersDir(), digest.Encoded()))
 	}
 
@@ -154,7 +108,45 @@ func (i *Image) LayersDir() string {
 	return filepath.Join(i.BaseDir, "layers")
 }
 
-// isNameValid validates the name of an image.
-func isNameValid(name string) bool {
-	return imageNamePattern.MatchString(name)
+// Refresh reloads the missing image properties (from disk).
+func (i *Image) Refresh() error {
+	if err := i.loadConfig(); err != nil {
+		return err
+	}
+	if err := i.loadManifest(); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// loadConfig loads the image config from disk.
+func (i *Image) loadConfig() error {
+	file := filepath.Join(i.ConfigFilePath())
+	data, err := os.ReadFile(file)
+	if err != nil {
+		return err
+	}
+
+	i.Config = new(imagespec.Image)
+	if err := json.Unmarshal(data, i.Config); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// loadManifest loads the manifest data of an image from disk.
+func (i *Image) loadManifest() error {
+	data, err := os.ReadFile(i.ManifestFilePath())
+	if err != nil {
+		return err
+	}
+
+	i.Manifest = new(imagespec.Manifest)
+	if err := json.Unmarshal(data, i.Manifest); err != nil {
+		return err
+	}
+
+	return nil
 }
