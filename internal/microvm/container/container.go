5b5d7bcd1fc7687aa8a23cfc84b46c6ee1539c41
William Durand <will+git@drnd.me>
2022-07-05 19:48:11 +0000

    microvm: some minor clean-ups

--- internal/microvm/container/container.go
+++ internal/microvm/container/container.go
@@ -46,21 +46,46 @@ func (c *MicrovmContainer) VirtiofsdSocketPath() string {
 	return filepath.Join(c.BaseDir, "virtiofsd.sock")
 }
 
-func (c *MicrovmContainer) AppendLine(debug bool) string {
-	debugStr := "0"
-	if debug {
-		debugStr = "1"
+func (c *MicrovmContainer) ArgsForQEMU(pidFile string, debug bool) []string {
+	return []string{
+		"-M", "microvm",
+		"-m", "512m",
+		"-no-acpi", "-no-reboot", "-no-user-config", "-nodefaults", "-display", "none",
+		"-device", "virtio-serial-device",
+		"-device", "virtconsole,chardev=virtiocon0",
+		"-chardev", fmt.Sprintf("socket,id=virtiofs0,path=%s", c.VirtiofsdSocketPath()),
+		"-device", "vhost-user-fs-device,queue-size=1024,chardev=virtiofs0,tag=/dev/root",
+		// TODO: fixme
+		"-kernel", "/workspace/containers/microvm/build/vmlinux",
+		"-object", fmt.Sprintf("memory-backend-file,id=mem,size=%s,mem-path=%s,share=on", "512m", filepath.Join(c.BaseDir, "shm")),
+		"-numa", "node,memdev=mem",
+		"-pidfile", pidFile, "-daemonize",
+		"-append", c.appendLine(debug),
+	}
+}
+
+func (c *MicrovmContainer) appendLine(debug bool) string {
+	args := []string{
+		// Issue a keyboard controller reset to reboot. It's fine to reboot
+		// because we pass `-no-reboot` to QEMU.
+		"reboot=k",
+		// We use virtio-fs for the root filesystem.
+		"rootfstype=virtiofs", "root=/dev/root", "rw",
+		// `hvc0` is the virtio-console configured when we start QEMU.
+		"console=hvc0",
 	}
 
-	return strings.Join(append(
-		[]string{
-			"quiet", "reboot=t",
-			"rootfstype=virtiofs", "root=/dev/root", "rw",
-			"console=hvc0",
-			fmt.Sprintf("MV_DEBUG=%s", debugStr),
-			fmt.Sprintf("MV_HOSTNAME=%s", c.ID()),
-			fmt.Sprintf("MV_INIT=%s", strings.Join(c.Spec.Process.Args, " ")),
-		},
-		c.Spec.Process.Env...,
-	), " ")
+	if debug {
+		args = append(args, "MV_DEBUG=1")
+	} else {
+		args = append(args, "quiet", "MV_DEBUG=0")
+	}
+
+	args = append(
+		args,
+		fmt.Sprintf("MV_HOSTNAME=%s", c.Spec.Hostname),
+		fmt.Sprintf("MV_INIT=%s", strings.Join(c.Spec.Process.Args, " ")),
+	)
+
+	return strings.Join(append(args, c.Spec.Process.Env...), " ")
 }
